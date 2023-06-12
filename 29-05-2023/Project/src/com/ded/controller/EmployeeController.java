652da5ef4d6cc801a5356db003f1d0917532f917
Monica-j-k <monicajk2007@gmail.com>
2023-06-12 15:14:42 +0530

    Day6

--- 29-05-2023/Project/src/com/ded/controller/EmployeeController.java
+++ 29-05-2023/Project/src/com/ded/controller/EmployeeController.java
@@ -7,10 +7,13 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Collections;
 
 import com.ded.model.Employee;
+import com.ded.dao.EmployeeDao;
 
 public class EmployeeController implements EmployeeInterface {
 	
@@ -19,6 +22,7 @@ public class EmployeeController implements EmployeeInterface {
 	Employee emp;
 	
 	List<Employee> emplist = new ArrayList<>();
+	EmployeeDao empdao = new EmployeeDao();
 	
 	public void addEmployee()
 	{
@@ -33,6 +37,8 @@ public class EmployeeController implements EmployeeInterface {
 		System.out.println(emp.getEname());
 		
 		emplist.add(emp);
+		
+		empdao.insertEmployee(emp);
 	}
 	
 	public void viewEmployee() {
@@ -40,6 +46,14 @@ public class EmployeeController implements EmployeeInterface {
 //		System.out.println(emp.getEname());
 		
 		System.out.println(emplist);
+		
+		Iterator<Employee> i = emplist.iterator();
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
+		
+		System.out.println("Datas from DB:");
+		empdao.showEmployee();
 	}
 	
 	public void SerialEg() {
@@ -100,4 +114,42 @@ public class EmployeeController implements EmployeeInterface {
 			}
 
 	}
+	
+	public void updateEmployee() {
+		System.out.println("Enter the employee number to update: ");
+		int eno = sc.nextInt();
+		
+		System.out.println("Enter new employee name to update");
+		String ename = sc.next();
+		empdao.updateEmployee(eno, ename);
+	}
+	
+	public void deleteEmployee() {
+		System.out.println("Enter the employee number to delete: ");
+		int eno = sc.nextInt();
+		
+		empdao.deleteEmployee(eno);
+	}
+	
+	public List<Employee> getEmplist() {
+		// TODO Auto-generated method stub
+		return emplist;
+	}
+	
+	public void sortEmployee() {
+		System.out.println("Sort by name(1) or sort by Eno(2)?");
+		int ch = sc.nextInt();
+		
+		if(ch == 1) {
+			Collections.sort(getEmplist(),Employee.nameComparator);
+			empdao.sortEmployee(ch);
+		}
+		else {
+			Collections.sort(getEmplist());
+			empdao.sortEmployee(ch);
+		}
+		
+	}
+
+
 }
--- 29-05-2023/Project/src/com/ded/view/MainClass.java
+++ 29-05-2023/Project/src/com/ded/view/MainClass.java
@@ -3,6 +3,8 @@ package com.ded.view;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.io.IOException;
+import java.util.function.BiPredicate;
 
 import com.ded.controller.EmployeeController;
 
@@ -30,8 +32,8 @@ public class MainClass {
 			un = br.readLine();
 			System.out.println("Enter Password");
 			pwd = br.readLine();
-
-			if (un.equals("Deeps") && pwd.equals("pass")) {
+			BiPredicate<String, String> res = (u, p) -> u.equals(p);
+			if (res.test(pwd, un)) {
 				System.out.println("Welcome " + un);
 				System.out.println("Please wait.....loading");
 				Thread.sleep(5000);
@@ -42,6 +44,9 @@ public class MainClass {
 		System.out.println("2. View Employee");
 		System.out.println("3. Serialize");
 		System.out.println("4. DeSerialize");
+		System.out.println("5. Update Employee");
+		System.out.println("6. Delete Employee");
+		System.out.println("7. Sort Employee");
 		int choice = sc.nextInt();
 		switch (choice) {
 		case 1: {
@@ -51,10 +56,10 @@ public class MainClass {
 
 		case 2: {
 			ec.viewEmployee();
-			break;
+			break; 
 		}
 		
-		case 3: {
+		case 3: { 
 			ec.SerialEg();
 			break;
 		}
@@ -63,7 +68,23 @@ public class MainClass {
 			ec.DeSerialEg();
 			break;
 		}
-
+		
+		case 5:{
+			ec.updateEmployee();
+			break;
+		}
+			
+		case 6:{
+			ec.deleteEmployee();
+//			ec.viewEmployee();
+			break;
+		}
+		case 7:{
+			ec.sortEmployee();
+//			ec.viewEmployee();
+			break; 
+		}
+		
 		default: {
 			System.out.println("Enter a valid number");
 			break;
